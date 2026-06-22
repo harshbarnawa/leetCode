3366beb848bc29036c383844f07af2a6cfe6d7f3
Harsh Barnawa <harshbarnawa.info@gmail.com>
2026-06-22 20:33:15 +0530

    implemented freq concept in daily quest of lc

---
+++ Easy/Vector/maxballoons.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxNumberOfBalloons(string text) {
+    vector<int> freq(26, 0);
+
+    for (char ch : text)
+        freq[ch - 'a']++;
+
+    return min({
+        freq['b' - 'a'],
+        freq['a' - 'a'],
+        freq['l' - 'a'] / 2,
+        freq['o' - 'a'] / 2,
+        freq['n' - 'a']
+    });
+}
+
+int main() {
+    string text;
+    cin >> text;
+
+    cout << maxNumberOfBalloons(text);
+
+    return 0;
+}
