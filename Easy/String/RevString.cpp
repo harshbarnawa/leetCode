4234e218dee3988cf0307d160ba047db0a4d2ff4
Harsh Barnawa <harshbarnawa.info@gmail.com>
2026-06-22 20:43:57 +0530

    known the concept but bit confused

---
+++ Easy/String/RevString.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void reverseString(vector<char>& s) {
+    int left = 0;
+    int right = s.size() - 1;
+
+    while (left < right) {
+        swap(s[left], s[right]);
+        left++;
+        right--;
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<char> s(n);
+
+    for (int i = 0; i < n; i++)
+        cin >> s[i];
+
+    reverseString(s);
+
+    for (char ch : s)
+        cout << ch << " ";
+
+    return 0;
+}
